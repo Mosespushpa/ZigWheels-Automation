d90f51941ac6fc81077ba07bf616d9966b529b20
Gonepally <2457219@cognizant.com>
2026-01-23 09:54:31 +0530

    Integrated Cross browser and Parallel Testing

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -8,20 +8,48 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 
     public WebDriver driver;
     public Logger logger;
 
+    @Parameters("browser")
     @BeforeClass
-    public void setup() {
+    public void setup(@Optional("chrome") String browser) {
         logger = LogManager.getLogger(this.getClass());
-        ChromeOptions op = new ChromeOptions();
-        op.addArguments("--disable-notifications");
-        driver = new ChromeDriver(op);
+
+        switch (browser.toLowerCase()){
+            case "chrome":
+                ChromeOptions op = new ChromeOptions();
+                op.addArguments("--disable-notifications");
+                driver = new ChromeDriver(op);
+                break;
+
+            case "edge":
+                EdgeOptions eo = new EdgeOptions();
+                eo.addArguments("--disable-notifications");
+                driver = new EdgeDriver(eo);
+                break;
+
+            case "firefox":
+                FirefoxOptions fo = new FirefoxOptions();
+                fo.addArguments("--disable-notifications");
+                driver = new FirefoxDriver(fo);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Browser not supported: " + browser);
+        }
+
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
