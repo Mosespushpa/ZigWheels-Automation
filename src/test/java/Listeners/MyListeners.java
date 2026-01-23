79d249c89614b3ad78eeb97f5210de5293e0e0e9
Chinnappan <2457247@cognizant.com>
2026-01-23 13:56:32 +0530

    Modified Screenshot file path

--- src/test/java/Listeners/MyListeners.java
+++ src/test/java/Listeners/MyListeners.java
@@ -38,7 +38,7 @@ public class MyListeners implements ITestListener {
     public void onTestSuccess(ITestResult result){
         test = extent.createTest(result.getName());
         if(result.getName().equals("invalidUserDetails")){
-            test.addScreenCaptureFromPath("C:\\My WorkSpace\\Hackathon Project\\ZigWheels-Automation\\ScreenShots\\errorMessage.png");
+            test.addScreenCaptureFromPath(System.getProperty("user.dir") + "/screenShots/errorMessage.png");
         }
         test.log(Status.PASS, "Test case PASSED is: " +result.getName());
     }
